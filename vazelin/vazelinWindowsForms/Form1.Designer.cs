13b6d55391c84c06c225ee12e0598174f29cd0e4
Alexey Rusakov <rusal@bk.ru>
2021-09-02 13:19:00 +0300

    add WF app

---
+++ vazelin/vazelinWindowsForms/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace vazelinWindowsForms
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = global::vazelinWindowsForms.Properties.Resources._111;
+      this.pictureBox1.Location = new System.Drawing.Point(12, 204);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(1137, 428);
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(850, 132);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(214, 52);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(12, 12);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(1137, 92);
+      this.progressBar1.TabIndex = 3;
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 20;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // webBrowser1
+      // 
+      this.webBrowser1.Location = new System.Drawing.Point(403, 204);
+      this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+      this.webBrowser1.Name = "webBrowser1";
+      this.webBrowser1.Size = new System.Drawing.Size(746, 353);
+      this.webBrowser1.TabIndex = 4;
+      this.webBrowser1.Url = new System.Uri("https://voenkomats.ru/izmaylovo/", System.UriKind.Absolute);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1173, 644);
+      this.Controls.Add(this.webBrowser1);
+      this.Controls.Add(this.progressBar1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.pictureBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.Windows.Forms.Timer timer1;
+    private System.Windows.Forms.WebBrowser webBrowser1;
+  }
+}
+
---
+++ vazelin/vazelinWindowsForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vazelinWindowsForms
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (progressBar1.Value>=11)
+         progressBar1.Value-=10;
+      
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      progressBar1.Value++;
+      if (progressBar1.Value >= 99)
+        progressBar1.Value = 0;
+    }
+  }
+}
